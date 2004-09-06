0438b0a54277679306747860d1015e167768763e
Hilbert Schraal <hilbert@hippe-it.nl>
2004-09-06 16:53:20 +0000

    forget constructors; corrected spelling error in license

--- src/java/net/sf/sillyexceptions/OutOfTheBlueException.java
+++ src/java/net/sf/sillyexceptions/OutOfTheBlueException.java
@@ -1,5 +1,5 @@
 /*
-Silly exceptions - A library of funny sounding, but usefull Java Exceptions.
+Silly exceptions - A library of funny sounding, but useful Java Exceptions.
 Copyright (C) 2004  W.H. Schraal, sillyexceptions.sf.net
 
 This library is free software; you can redistribute it and/or
@@ -34,8 +34,54 @@ package net.sf.sillyexceptions;
  * </pre>
  *
  * @author W.H. Schraal
- * @version $Id: OutOfTheBlueException.java,v 1.2 2004/09/05 17:01:24 hippe Exp $
+ * @version $Id: OutOfTheBlueException.java,v 1.3 2004/09/06 16:53:22 hippe Exp $
  * @since 1.0
  */
 public class OutOfTheBlueException extends RuntimeException {
+
+  /**
+   * Constructs a new OutOfTheBlueException with the specified detail message.
+   * The cause is not initialized, and may subsequently be initialized by a
+   * call to {@link #initCause}.
+   *
+   * @param   message   the detail message. The detail message is saved for
+   *          later retrieval by the {@link #getMessage()} method.
+   */
+  public OutOfTheBlueException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new OutOfTheBlueException with the specified detail message
+   * and cause.  <p>Note that the detail message associated with
+   * <code>cause</code> is <i>not</i> automatically incorporated in
+   * this exception's detail message.
+   *
+   * @param  message the detail message (which is saved for later retrieval
+   *         by the {@link #getMessage()} method).
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
+   */
+  public OutOfTheBlueException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  /**
+   * Constructs a new OutOfTheBlueException with the specified cause and a
+   * detail message of <tt>(cause==null ? null : cause.toString())</tt>
+   * (which typically contains the class and detail message of
+   * <tt>cause</tt>).  This constructor is useful for exceptions
+   * that are little more than wrappers for other throwables.
+   *
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
+   */
+  public OutOfTheBlueException(Throwable cause) {
+    super(cause);
+  }
+
 }
